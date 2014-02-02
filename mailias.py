dfb65c84ccbe8b2af591437e3f808244cc479b1b
Sota Sugiura <t11460ss@sfc.keio.ac.jp>
2014-02-02 18:17:48 +0900

    fix mailias.py / ホームディレクトリをosモジュールで取得

--- mailias.py
+++ mailias.py
@@ -3,12 +3,14 @@
 # Date 2014/02/02
 
 import sys
+import os
 
 # .zshrcに追加するalias
 argvs = sys.argv
 insert = "alias "+argvs[2]+"=\"cd "+argvs[1]+"\""
+home = os.environ['HOME']
 
 # 書き込み
-f = open('~/.zshrc','a+')
+f = open(home+'/.zshrc','a+')
 f.write(insert)
 f.close()
